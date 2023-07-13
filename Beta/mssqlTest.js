a741e462747fc87f17357e225fec6a4120874473
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-07-13 11:46:04 -0700

    Finally, a working version.

--- Beta/mssqlTest.js
+++ Beta/mssqlTest.js
@@ -2,39 +2,40 @@ const sql = require('mssql');
 const msnodesqlv8 = require('msnodesqlv8');
 const config = require('./config.json');
 
-const conn = {
-    database:config.mssql.database,
-    server:config.mssql.server,
-    user:config.mssql.user,
-    password:config.mssql.password,
-    options: {
-        trustedConnection: true,
-        trustServerCertificate: true
-    }
-};
-
-sql.connect(conn).then(() => {
-    console.log("Connection successful!");
-    const request = new sql.Request();
-    request.query('SELECT * FROM Staff').then((recordset) => {
-        console.log(recordset);
-        sql.close();
-    }).catch((err) => {
-         console.error(err);
-         sql.close();
-    });
-}).catch((err) => {
-    console.error(err);
-});
-
-// const connectionString = "server=.;Database=master;Trusted_Connection=Yes;Driver={ODBC Driver 18 for SQL Server}";
-// const query = "SELECT * FROM dbo.Staff";
-
-// msnodesqlv8.query(connectionString, query, (err, rows) => {
-//     if(err) {
-//         console.error(err);
+// const conn = {
+//     driver:config.mssql.driver,
+//     database:config.mssql.database,
+//     server:config.mssql.server,
+//     user:config.mssql.user,
+//     password:config.mssql.password,
+//     options: {
+//         trustedConnection: true,
+//         trustServerCertificate: true
 //     }
-//     else {
-//         console.log(rows);
-//     }
+// };
+
+// sql.connect(conn).then(() => {
+//     console.log("Connection successful!");
+//     const request = new sql.Request();
+//     request.query('SELECT * FROM Staff').then((recordset) => {
+//         console.log(recordset);
+//         sql.close();
+//     }).catch((err) => {
+//          console.error(err);
+//          sql.close();
+//     });
+// }).catch((err) => {
+//     console.error(err);
 // });
+
+const connectionString = "server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}";
+const query = "SELECT * FROM master.dbo.Staff";
+
+msnodesqlv8.query(connectionString, query, (err, rows) => {
+    if(err) {
+        console.error(err);
+    }
+    else {
+        console.log(rows);
+    }
+});
