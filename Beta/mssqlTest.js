91839b75c613d23f1882217e5ce58d10fe1d86de
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-07-14 08:35:37 -0700

    Installed package node-adodb.

--- Beta/mssqlTest.js
+++ Beta/mssqlTest.js
@@ -1,32 +1,6 @@
-// const sql = require('mssql');
 const msnodesqlv8 = require('msnodesqlv8');
+const ADODB = require('node-adodb')
 // const config = require('./config.json');
-
-// const conn = {
-//     driver:config.mssql.driver,
-//     database:config.mssql.database,
-//     server:config.mssql.server,
-//     user:config.mssql.user,
-//     password:config.mssql.password,
-//     options: {
-//         trustedConnection: true,
-//         trustServerCertificate: true
-//     }
-// };
-
-// sql.connect(conn).then(() => {
-//     console.log("Connection successful!");
-//     const request = new sql.Request();
-//     request.query('SELECT * FROM Staff').then((recordset) => {
-//         console.log(recordset);
-//         sql.close();
-//     }).catch((err) => {
-//          console.error(err);
-//          sql.close();
-//     });
-// }).catch((err) => {
-//     console.error(err);
-// });
 
 const connectionString = `server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}`;
 const query = "SELECT * FROM master.dbo.Staff";
