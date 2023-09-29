463aed9cf910412ecd17f81ff75613b16ec4d365
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-09-29 15:55:41 -0700

    Tesing how to connect using mssql library.

---
+++ Beta/mssql_test.js
@@ -0,0 +1,38 @@
+sql = require('mssql');
+jsonData = require('./config.json');
+
+const config = {
+    user: jsonData.mssql.user,
+    password: jsonData.mssql.password,
+    server: jsonData.mssql.server,
+    database: jsonData.mssql.database,
+    domain: jsonData.mssql.domain,
+    options : jsonData.mssql.options
+};
+// const pool = new msnodesqlv8.Pool({
+//     connectionString: connectionString
+// });
+const pool = new sql.ConnectionPool(config);
+
+// connect to your database
+sql.connect(config, function (err) {
+    
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Success!");
+        // create Request object
+    var request = new sql.Request();
+       
+    // query to the database and get the records
+    request.query('SELECT * from Staff', function (err, recordset) {
+        
+        if (err) console.log(err)
+
+        // send records as a response
+        res.send(recordset);
+        
+    });
+    }
+});
