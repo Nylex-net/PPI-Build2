f76c5e0b287d3e1ff2a1cc09d42c46b42a231422
henatic <hdmorgan4@gmail.com>
2023-10-03 09:10:05 -0700

    Change DB path.

--- Server/mssqlPop2.js
+++ Server/mssqlPop2.js
@@ -2,7 +2,7 @@ const sql = require('mssql');
 const ADODB = require('node-adodb')
 // const Pool = require('generic-pool');
 // const config = require('./config.json');
-const DATABASE_PATH = "C:\\Users\\administrator\\Documents\\PPI\\Database\\SHN_Project_Backup.mdb;";
+const DATABASE_PATH = "C:\\Users\\administrator\\Documents\\SHN_Project_Backup.mdb;";
 // const query = "SELECT * FROM master.dbo.Staff";
 const connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source='+DATABASE_PATH);
 const jsonData = require('./config.json');
