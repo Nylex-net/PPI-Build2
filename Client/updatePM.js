fb9d52e91ff251773e6e608c9f046a928b624d87
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-10-31 16:50:39 -0700

    Client side PM.

--- Client/updatePM.js
+++ Client/updatePM.js
@@ -1,6 +1,8 @@
 function upload() {
     const formData = new FormData();
-    formData.append('file', document.getElementById('task1').files[0]);
+    formData.append('file1', document.getElementById('task1').files[0]);
+    formData.append('file5', document.getElementById('task5').files[0]);
+    formData.append('file10', document.getElementById('task10').files[0]);
     fetch('https://e-dt-usertest.shn-engr.com:3001/upload', {
         method: 'POST',
         body: formData,
