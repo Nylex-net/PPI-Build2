99b3f2ebe16d58b06bff1020ded72a94535bcf5f
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-10-31 15:19:35 -0700

    Proper url

--- Client/updatePM.js
+++ Client/updatePM.js
@@ -1,7 +1,7 @@
 function upload() {
     const formData = new FormData();
     formData.append('file', document.getElementById('task1').files[0]);
-    fetch('https://e-dt-usertest.shn-engr.com:3001', {
+    fetch('https://e-dt-usertest.shn-engr.com:3001/upload', {
         method: 'POST',
         body: formData,
     })
