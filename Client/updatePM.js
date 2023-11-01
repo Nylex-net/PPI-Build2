05623471d4a4cbf6e852c0361d67122ca8c70f5f
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-11-01 09:10:56 -0700

    Testing new submit method.

--- Client/updatePM.js
+++ Client/updatePM.js
@@ -1,8 +1,28 @@
-function upload() {
+// function upload() {
+//     const formData = new FormData();
+//     formData.append('file', document.getElementById('task1').files[0]);
+//     formData.append('file', document.getElementById('task5').files[0]);
+//     formData.append('file', document.getElementById('task10').files[0]);
+//     fetch('https://e-dt-usertest.shn-engr.com:3001/upload', {
+//         method: 'POST',
+//         body: formData,
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// }
+
+document.getElementById('projForm').addEventListener('submit', function (e) {
+    e.preventDefault();
     const formData = new FormData();
     formData.append('file', document.getElementById('task1').files[0]);
     formData.append('file', document.getElementById('task5').files[0]);
     formData.append('file', document.getElementById('task10').files[0]);
+
     fetch('https://e-dt-usertest.shn-engr.com:3001/upload', {
         method: 'POST',
         body: formData,
@@ -14,4 +34,4 @@ function upload() {
     .catch(error => {
         console.error(error);
     });
-}
+});
