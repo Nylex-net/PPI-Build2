0945bb1087437a9d968902470f4aee09b89d4c79
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-11-07 16:15:31 -0800

    Require login to upload files to SharePoint.

--- Client/updatePM.js
+++ Client/updatePM.js
@@ -1,34 +1,40 @@
-// function upload() {
-//     const formData = new FormData();
-//     formData.append('file', document.getElementById('task1').files[0]);
-//     formData.append('file', document.getElementById('task5').files[0]);
-//     formData.append('file', document.getElementById('task10').files[0]);
-//     fetch('https://e-dt-usertest.shn-engr.com:3001/upload', {
-//         method: 'POST',
-//         body: formData,
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.error(error);
-//     });
-// }
-
 document.getElementById('projForm').addEventListener('submit', function (e) {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('file', document.getElementById('task1').files[0]);
-    formData.append('file', document.getElementById('task5').files[0]);
-    formData.append('file', document.getElementById('task10').files[0]);
-
-    fetch('https://e-dt-usertest.shn-engr.com:3001/upload', {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => console.log(response))
-    .catch(error => {
-        console.error(error);
-    });
+    signIn();
 });
+
+function verify(id) {
+    signIn();
+}
+
+async function starter() {
+    const response = await fetch('https://'+HOST+'.shn-engr.com:3001/verify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ID:id})
+    });
+    if(await response.json().result) {
+        const formData = new FormData();
+        formData.append('file', document.getElementById('task1').files[0]);
+        formData.append('file', document.getElementById('task5').files[0]);
+        formData.append('file', document.getElementById('task10').files[0]);
+
+        fetch('https://e-dt-usertest.shn-engr.com:3001/upload', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => {
+            console.log(response);
+            document.getElementById('response').innerHTML = "Successful response. Double check the SharePoint site to make sure your files uploaded.";
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById('response').innerHTML = "An error ocurred.";
+        });
+    }
+    else {
+        document.getElementById('response').innerHTML = "You're not authorized to upload files to SharePoint.";
+    }
+}
