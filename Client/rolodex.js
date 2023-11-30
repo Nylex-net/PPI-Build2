89c86daf5fe85d3e23a6b2ac97f06717ee8eb406
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-11-30 09:51:08 -0800

    Formats the phone numbers.

--- Client/rolodex.js
+++ Client/rolodex.js
@@ -199,10 +199,10 @@ function starter(res) {
     '<div class="col-lg-4"><label for="city">City </label></div><div class="col-lg-8"><input type="text" id="city" name="city" maxlength="255" value="'+ json.city +'" required/></div>'+
     '<div class="col-lg-4"><label for="state">State </label></div><div class="col-lg-8"><select name="state" id="state" size="1" required><option value="AL">Alabama</option><option value="AK">Alaska</option><option value="AZ">Arizona</option><option value="AR">Arkansas</option><option value="CA" selected="selected">California</option><option value="CO">Colorado</option><option value="CT">Connecticut</option><option value="DE">Delaware</option><option value="DC">Dist of Columbia</option><option value="FL">Florida</option><option value="GA">Georgia</option><option value="HI">Hawaii</option><option value="ID">Idaho</option><option value="IL">Illinois</option><option value="IN">Indiana</option><option value="IA">Iowa</option><option value="KS">Kansas</option><option value="KY">Kentucky</option><option value="LA">Louisiana</option><option value="ME">Maine</option><option value="MD">Maryland</option><option value="MA">Massachusetts</option><option value="MI">Michigan</option><option value="MN">Minnesota</option><option value="MS">Mississippi</option><option value="MO">Missouri</option><option value="MT">Montana</option><option value="NE">Nebraska</option><option value="NV">Nevada</option><option value="NH">New Hampshire</option><option value="NJ">New Jersey</option><option value="NM">New Mexico</option><option value="NY">New York</option><option value="NC">North Carolina</option><option value="ND">North Dakota</option><option value="OH">Ohio</option><option value="OK">Oklahoma</option><option value="OR">Oregon</option><option value="PA">Pennsylvania</option><option value="RI">Rhode Island</option><option value="SC">South Carolina</option><option value="SD">South Dakota</option><option value="TN">Tennessee</option><option value="TX">Texas</option><option value="UT">Utah</option><option value="VT">Vermont</option><option value="VA">Virginia</option><option value="WA">Washington</option><option value="WV">West Virginia</option><option value="WI">Wisconsin</option><option value="WY">Wyoming</option></select></div>'+
     '<div class="col-lg-4"><label for="zip">Zip </label></div><div class="col-lg-8"><input type="text" id="zip" name="zip" maxlength="20" value="'+ json.zip_code +'" required></div>'+
-    '<div class="col-lg-4"><label for="WP">Work Phone </label></div><div class="col-lg-8"><input type="tel" id="WP" name="WP" maxlength="20" value="'+json.work_phone+'"required></div>'+
-    '<div class="col-lg-4"><label for="HP">Home Phone </label></div><div class="col-lg-8"><input type="tel" id="HP" name="HP" value="'+json.home_phone+'" maxlength="20"></div>'+
-    '<div class="col-lg-4"><label for="cell">Cell </label></div><div class="col-lg-8"><input type="tel" id="cell" name="cell" value="'+ json.cell +'" maxlength="20"></div>'+
-    '<div class="col-lg-4"><label for="fax">Fax </label></div><div class="col-lg-8"><input type="tel" id="fax" name="fax" value="'+ json.fax +'" maxlength="20"></div>'+
+    '<div class="col-lg-4"><label for="WP">Work Phone </label></div><div class="col-lg-8"><input type="tel" id="WP" name="WP" maxlength="12" value="'+json.work_phone+'"required></div>'+
+    '<div class="col-lg-4"><label for="HP">Home Phone </label></div><div class="col-lg-8"><input type="tel" id="HP" name="HP" value="'+json.home_phone+'" maxlength="12"></div>'+
+    '<div class="col-lg-4"><label for="cell">Cell </label></div><div class="col-lg-8"><input type="tel" id="cell" name="cell" value="'+ json.cell +'" maxlength="12"></div>'+
+    '<div class="col-lg-4"><label for="fax">Fax </label></div><div class="col-lg-8"><input type="tel" id="fax" name="fax" value="'+ json.fax +'" maxlength="12"></div>'+
     '<div class="col-lg-4"><label for="email">Email </label></div><div class="col-lg-8"><input type="email" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" maxlength="75" value="'+ json.email +'" required></div>'+
     '</div></div>'+
     '<div id="submitter"><button type="button" onclick="back()">Back</button><button type="button" onclick="preparePost('+json.ID+', '+isProject+')">Submit</button></div>';
@@ -217,6 +217,62 @@ function starter(res) {
         }
     }
     window.scrollTo(0,0);
+
+    const workPhoneInput = document.getElementById('WP');
+    const homePhoneInput = document.getElementById('HP');
+    const cellPhoneInput = document.getElementById('cell');
+    const faxInput = document.getElementById('fax');
+
+    workPhoneInput.addEventListener('input', function () {
+        // Remove any non-numeric characters
+        const cleanedValue = this.value.replace(/\D/g, '');
+        // Format the phone number with dashes
+        const formattedValue = formatPhoneNumber(cleanedValue);
+        // Set the formatted value back to the input field
+        this.value = formattedValue;
+    });
+
+    homePhoneInput.addEventListener('input', function () {
+        // Remove any non-numeric characters
+        const cleanedValue = this.value.replace(/\D/g, '');
+        // Format the phone number with dashes
+        const formattedValue = formatPhoneNumber(cleanedValue);
+        // Set the formatted value back to the input field
+        this.value = formattedValue;
+    });
+
+    cellPhoneInput.addEventListener('input', function () {
+        // Remove any non-numeric characters
+        const cleanedValue = this.value.replace(/\D/g, '');
+        // Format the phone number with dashes
+        const formattedValue = formatPhoneNumber(cleanedValue);
+        // Set the formatted value back to the input field
+        this.value = formattedValue;
+    });
+
+    faxInput.addEventListener('input', function () {
+        // Remove any non-numeric characters
+        const cleanedValue = this.value.replace(/\D/g, '');
+        // Format the phone number with dashes
+        const formattedValue = formatPhoneNumber(cleanedValue);
+        // Set the formatted value back to the input field
+        this.value = formattedValue;
+    });
+}
+
+/**
+ * 
+ * @param {String} phoneNumber 
+ * @returns String in phone number format.
+ */
+
+function formatPhoneNumber(phoneNumber) {
+    const match = phoneNumber.match(/^(\d{0,3})(\d{0,3})(\d{0,4})$/);
+    if (match) {
+        const parts = match.slice(1).filter(Boolean);
+        return parts.join('-');
+    }
+    return phoneNumber;
 }
 
 function back() {
