ef2876035e8150a72f2e77486e0cb81288f36351
Henry Morgan <76470485+henatic@users.noreply.github.com>
2024-03-29 09:06:46 -0700

    I don't know why I was trying to set the array length to 0.

--- Client/prevailing_wage.js
+++ Client/prevailing_wage.js
@@ -37,7 +37,7 @@ function listWage(admin) {
     }).then(data => {
         document.getElementById("results").innerHTML = '<p>Processing...</p>';
         let table = '<tr><th>Project</th><th>Billing Groups</th><th>Office</th>'+(admin?'<th>Display</th><th>Edit</th>':'')+'</tr>';
-        existing.length = 0;
+        // existing.length = 0;
         for(let json = 0; json < data.length; json++) {
             table += '<tr><td>'+ data[json].project_id + '</td><td>' + (data[json].BillGrp != 'NULL' && data[json].BillGrp != null?data[json].BillGrp:'') + '</td><td>'+
             (data[json].office == 2?'klamath Falls':(data[json].office == 4?'Willits':(data[json].office == 5?'Redding':(data[json].office == 6?'Coos Bay':'Eureka')))) +
